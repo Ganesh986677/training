1ff450e634b026147ae21631103ac08f5e5b2660
Ganesh986677 <ganeshboa4@outlook.com>
2024-11-22 16:29:25 +0530

    modified key path

--- output.tf
+++ output.tf
@@ -1,7 +1,3 @@
-output "vm_public_ip" {
+output "vm-public-ip" {
   value = azurerm_public_ip.training.ip_address
-}
-
-output "Playbook_execution_log" {
-  value = ansible_playbook.webapp.ansible_playbook_stdout
 }
